383a9375450253db3efdc18205332511cc1e0252
ranktel <ranktel@worktop.myguest.virtualbox.org>
2023-04-20 16:35:09 -0400

    complete

--- index.js
+++ index.js
@@ -6,10 +6,59 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
-  try {
+
+try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
+console.log(req.query)
+var content  =  req.query["content"];
+var id = req.query["tags"]
+
+
+var jokes =[]
+if(content == undefined){
+	if(id == undefined){
+		 jokes = await Joke.findAll();
+	}else{
+	
+		var allContent = await Joke.findAll();
+       		 for(let i = 0 ; i  < allContent.length; i++){
+                	if(allContent[i].tags.includes(id)){
+		 		jokes.push(allContent[i]);
+				console.log("nice")
+                	}
+        	}
+
+	}
+}else{
+	var allContent = await Joke.findAll();
+	for(let i = 0 ; i  < allContent.length; i++){
+		if(allContent[i].joke.includes(content)){
+			content = allContent[i].joke;
+			break;
+		}
+	}
+	if(id == undefined){
+		jokes = await Joke.findAll(
+			{
+				where: {
+					joke: content
+				}
+			}
+		);
+	}else{
+		var allContent = await Joke.findAll();
+                 for(let i = 0 ; i  < allContent.length; i++){
+                        if(allContent[i].tags.includes(id)){
+                                jokes.push(allContent[i]);
+                                console.log("nice")
+                        }
+                }
+
+	
+	}
+}
+   	
+	  res.send(jokes);
   } catch (error) {
     console.error(error);
     next(error)
